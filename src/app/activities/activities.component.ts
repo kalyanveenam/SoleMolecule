f6a3b8791e70d02218138c6e9789d6dc2efb2137
unknown <kalyanveenam@gmail.com>
2020-07-09 00:39:42 +0530

    Changed home page UI

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -6,23 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activities.component.css'],
 })
 export class ActivitiesComponent implements OnInit {
-  public activities = [
-    {
-      ActivityName: 'kayaking',
-      image: '../../assets/kayak.jpg',
-      Description: 'This is sample description',
-    },
-    {
-      ActivityName: 'Fish your way',
-      image: '../../assets/fishing.jpg',
-      Description: 'This is sample description',
-    },
-    {
-      ActivityName: 'Hang in and have some drinks',
-      image: '../../assets/drinks.jpeg',
-      Description: 'This is sample description',
-    },
-  ];
+  
 
   constructor() {}
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,28 +3,52 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   title = 'ngSlick';
 
-
   slides = [1, 2, 3, 4];
 
   slideConfig = {
-    "slidesToShow": 1,
-    "slidesToScroll": 1,
-    "nextArrow": "<div class='nav-btn next-slide'></div>",
-    "prevArrow": "<div class='nav-btn prev-slide'></div>",
-    "dots": true,
-    "autoplay": true,
-    "infinite": false
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: "<div class='nav-btn next-slide'></div>",
+    prevArrow: "<div class='nav-btn prev-slide'></div>",
+    dots: true,
+    autoplay: true,
+    infinite: false,
   };
 
-  constructor() { }
-
-  ngOnInit(): void {
-    
+  constructor() {}
+  public activities = [
+    {
+      ActivityName: 'kayaking',
+      image: '../../assets/kayak.jpg',
+      Description: 'This is sample description',
+    },
+    {
+      ActivityName: 'Fish your way',
+      image: '../../assets/fishing.jpg',
+      Description: 'This is sample description',
+    },
+    {
+      ActivityName: 'Hang in and have some drinks',
+      image: '../../assets/drinks.jpeg',
+      Description: 'This is sample description',
+    },
+  ];
+  slickInit(e) {
+    console.log('slick initialized');
   }
-
+  breakpoint(e) {
+    console.log('breakpoint');
+  }
+  afterChange(e) {
+    console.log('afterChange');
+  }
+  beforeChange(e) {
+    console.log('beforeChange');
+  }
+  ngOnInit(): void {}
 }
