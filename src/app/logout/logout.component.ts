e90309ce2d808b83395e35630c5efa5eaeb07fb3
karthik.veenam <karthik.veenam@servicenow.com>
2020-08-15 18:28:40 +0530

    Logout component

--- src/app/logout/logout.component.ts
+++ src/app/logout/logout.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { HttpConnect } from '../login/httpconnect.service';
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.css']
 })
 export class LogoutComponent implements OnInit {
-@Input() name:string;
+name:string;
+@Input() isMobile;
+isLoggedIn:boolean;
+constructor(private httpConnect : HttpConnect){
+  this.isLoggedIn = (localStorage.getItem("isLoggedIn") == 'true');
+  this.name = localStorage.getItem("name");
+  this.isMobile = (this.isMobile=="true");
+}
+ngOnInit(){
+this.isLoggedIn = (localStorage.getItem("isLoggedIn") == 'true');
+this.name = localStorage.getItem("name");
+this.isMobile = (this.isMobile=="true");
+}
+  logout(){
+    var token = localStorage.getItem("token");
+    this.httpConnect.logout(token).subscribe((response) =>{
+    if(response){
+      console.log('res bis '+JSON.stringify(response));
+      localStorage.clear();
+      this.isLoggedIn = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    }
+    })
+}
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,26 +7,10 @@ import {HttpConnect} from '../login/httpconnect.service';
   providers:[HttpConnect]
 })
 export class NavbarComponent {
-  isLoggedIn:boolean;
-  name:string;
-  constructor(private httpConnect : HttpConnect){
-     this.isLoggedIn = (localStorage.getItem("isLoggedIn") == 'true');
-     this.name = localStorage.getItem("name");
+  constructor(){
   }
 ngInIt(){
-  this.isLoggedIn = (localStorage.getItem("isLoggedIn") == 'true');
-  this.name = localStorage.getItem("name");
 }
-logout(){
-var token = localStorage.getItem("token");
-this.httpConnect.logout(token).subscribe((response) =>{
-if(response){
-  console.log('res bis '+JSON.stringify(response));
-  this.isLoggedIn=false;
-  localStorage.clear();
 }
 
-})
-}
 
-}
